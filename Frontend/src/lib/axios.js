dc33592ce1750cba1cf9c326f2f5779bd58e4305
*vivek <vrvivekofficial@gmail.com>
2025-05-20 11:27:47 +0530

    *

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -3,8 +3,7 @@ import axios from "axios";
 console.log(import.meta.env.VITE_BACKEND_URL);
 console.log(import.meta.env);
 
-
 export const axiosInstance = axios.create({
-  baseURL: "https://bookstore-rpsf.onrender.com",
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
 });
