c66117fa1affa7086235895dac7adfa5eb8821d3
*vivek <vrvivekofficial@gmail.com>
2025-05-20 11:06:40 +0530

    *

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+console.log(import.meta.env.VITE_BACKEND_URL);
+console.log(import.meta.env);
+
+
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
